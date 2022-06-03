75936b1e55db63aeca35913ca964d0aaf0b2c7af
troy_p <troyparker55@gmail.com>
2022-06-03 09:53:17 -0500

    Tested JSON api

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const handleSubmit = (e) => {
 }
   useEffect(() => {
       console.log('Username: ' + userName + ' Password: ' + password);
-    });
+    }, []);
   
   if(loginToken == null) {
   return (
@@ -40,7 +40,11 @@ const handleSubmit = (e) => {
   
 }
   else {
-    return <h1>You are logged in!</h1>;
+    return (
+        <div>
+            <h1>You are logged in!</h1> 
+          
+        </div>);
   }};
 
 function App() {
@@ -48,11 +52,98 @@ function App() {
     <div className="App">
       <div>
         <Hello />
-        
+        <Pets />
       </div>
 
     </div>
   );
+}
+
+function Pets() {
+  const [coolPets, setCoolPets] = useState([]);
+  
+
+  useEffect(() => {
+    console.log(coolPets.length)
+    fetch('http://localhost:3000/animals2.json')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setCoolPets(data);
+        // console.log(Object.keys(coolPets))
+        coolPets.map((el) => {
+          let firstKey = Object.keys(el)[0];
+                console.log(firstKey);
+              })
+      })
+  }, [])
+
+
+  // FOR USE WITH ANIMALS.JSON
+  // const allThePets = () => {
+  //   if(coolPets.length > 0) {
+  //     const petsMap = coolPets.map((el) => {
+  //       let firstKey = Object.keys(el)[0];
+  //             return(
+  //               <div key={firstKey}> 
+  //                 {firstKey}
+  //               </div>)
+  //           })
+  //     return (<div>
+  //       some pets found {petsMap}
+  //     </div>)
+  //   } else {
+  //     return(
+  //     <div>no pets found</div>
+  //     )
+  //   }
+  // }
+
+  const allThePets = () => {
+    if(coolPets.length > 0) {
+      const petsMap = coolPets.map((el) => {
+        let keyString = '';
+        Object.keys(el).filter((k) => {
+          return  k != "name";
+
+        //  Using Key Lookup
+        }).map((attr_key) => {
+          keyString = keyString + attr_key + ': '+ el[attr_key] + ', ';
+        })
+      
+        // keyString = keyString.slice(0, -2);
+        // const attrMap = el.map((attr) => {
+        //   return (
+        //   <div className="petAttribute">
+        //     <div className='petAttributeKey'>{attr}</div>
+        //     <div className='petAttributeValue'>{el[attr]}</div>
+        //   </div>
+        //   )
+        // })
+              return(
+                <div className='petBox' key={el.name}> 
+                  <div className='petName'>{el.name}</div>
+                  <div className='petAttributes'>{keyString}</div>
+                </div>)
+            })
+      return (<div>
+        some pets found {petsMap}
+      </div>)
+    } else {
+      return(
+      <div>no pets found</div>
+      )
+    }
+  }
+
+  return (
+    <div>
+    <h1>Here are the pets!</h1>
+    {allThePets()}
+    </div>
+  );
+   
 }
 
 export default App;
